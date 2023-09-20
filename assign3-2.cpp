4f1f0461d125ceece8bc9a9f06f054458e90b02d
Jacksonfphan <144867440+Jacksonfphan@users.noreply.github.com>
2023-09-20 04:49:36 +0000

    assign3-2

--- assign3-2.cpp
+++ assign3-2.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
-
+/*Create a program that prints out the electricity charge for
+ a household based on the number of units consumed 
+ (units are received from the input). 
+ The rate is $0.12 per unit for
+ the first 100 units and $0.15 per unit for additional units.*/
 int main(){
 
 }
